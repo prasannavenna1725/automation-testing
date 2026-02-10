26f0c02f034e88d22b0204c373f0d8093a1f8d6d
prasannavenna1725 <prasannavenna1225@gmail.com>
2026-02-10 10:28:15 +0000

    Add files via upload

---
+++ driven_demo/src/test/java/bdd_demo/runner.java
@@ -0,0 +1,25 @@
+package bdd_demo;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+
+
+//@SuppressWarnings("deprecation")
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/feature/googlesearch",
+glue = {"stepdefinitions"} ,monochrome = true,
+
+plugin = {"pretty","html:target/HtmlReports.html","json:target/JSONReports"})
+		
+public class runner  extends AbstractTestNGCucumberTests{
+		
+}
+
+
+
+
---
+++ driven_demo/src/test/java/bdd_demo/search.java
@@ -0,0 +1,57 @@
+package bdd_demo;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.Keys;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	import io.cucumber.java.en.Given;
+	import io.cucumber.java.en.Then;
+	import io.cucumber.java.en.When;
+	public class search {
+	
+		WebDriver driver;
+		
+		@Given("browser is open")
+		public void browser_is_open() {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		    // Write code here that turns the phrase above into concrete actions
+		   // throw new io.cucumber.java.PendingException();
+		}
+
+		@Given("user is on google search page")
+		public void user_is_on_google_search_page() {
+			
+			driver.navigate().to("https://www.google.com/");
+		    // Write code here that turns the phrase above into concrete actions
+		   // throw new io.cucumber.java.PendingException();
+		}
+
+		@When("user enters a text in search box")
+		public void user_enters_a_text_in_search_box()throws InterruptedException
+		{
+			driver.findElement(By.name("q")).sendKeys("India");
+			
+			driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+			 
+			Thread.sleep(3000);
+		    // Write code here that turns the phrase above into concrete actions
+		    //throw new io.cucumber.java.PendingException();
+		}
+
+		@Then("user is navigated to search results")
+		public void user_is_navigated_to_search_results() {
+			
+			//driver.quit();
+		    // Write code here that turns the phrase above into concrete actions
+		   // throw new io.cucumber.java.PendingException();
+		}
+
+	}
+
+
+
+
+
+
