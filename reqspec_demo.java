c3008f38e75c7246c1fb7e0810db9eb8d1d6d550
prasannavenna1725 <prasannavenna1225@gmail.com>
2026-02-10 10:19:17 +0000

    Add files via upload

---
+++ reqspec_demo.java
@@ -0,0 +1,32 @@
+package restassured;
+
+
+	import org.testng.annotations.Test;
+
+	import io.restassured.RestAssured;
+	import io.restassured.builder.RequestSpecBuilder;
+	import io.restassured.response.Response;
+	import io.restassured.specification.RequestSpecification;
+
+	public class reqspec_demo {
+
+		@Test
+		void demo1()
+		{
+	        RequestSpecBuilder reqBuilder=new RequestSpecBuilder();
+			reqBuilder.setBaseUri("https://restful-booker.herokuapp.com");
+			reqBuilder.setBasePath("/booking");
+			
+	         RequestSpecification reqSpec=  reqBuilder.build();
+
+			System.out.println("------------------");
+			
+			Response res2=RestAssured.given(reqSpec).get();
+			System.out.println(res2.asString());
+			System.out.println("-----------");
+		}
+	}
+
+
+
+
